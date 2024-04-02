a1266395950dbf29199b2b71d85cd8715bccef72
HY.G <ghy_mike@163.com>
2024-04-02 19:34:06 +0800

    add map allocator

--- backtrace_helper/include/backtrace_helper/backtrace.h
+++ backtrace_helper/include/backtrace_helper/backtrace.h
@@ -48,6 +48,79 @@ public:
     void unlock() { m_state.clear(std::memory_order_release); }
 };
 
+template<typename T>
+class SysAlloc : public NonCopyable {
+public:
+    typedef size_t     size_type;
+    typedef ptrdiff_t  difference_type;
+    typedef T*         pointer;
+    typedef const T*   const_pointer;
+    typedef T&         reference;
+    typedef const T&   const_reference;
+    typedef T          value_type;
+
+    template<typename X>
+    struct rebind
+    { typedef SysAlloc<X> other; };
+
+    SysAlloc() throw() {
+        #define RESOLVE(name)                                                  \
+            do {                                                               \
+                auto addr = dlsym(RTLD_NEXT, #name);                           \
+                if (!addr) {                                                   \
+                    FAIL_EXIT("can not resolve %s: %s", #name, dlerror());     \
+                }                                                              \
+                sys_##name = reinterpret_cast<decltype(sys_##name)>(addr); \
+            } while (0)
+            RESOLVE(malloc);
+            RESOLVE(free);
+        #undef RESOLVE
+    }
+
+    template<typename X>
+    SysAlloc(const SysAlloc<X>&) throw() {}
+
+    SysAlloc(const SysAlloc&) = delete;
+    void operator=(const SysAlloc&) = delete;
+
+    ~SysAlloc() throw() {};
+
+    pointer address(reference x) const { return &x; }
+    const_pointer address(const_reference x) const { return &x; }
+
+    pointer allocate(size_type n, const void * hint = 0) {
+        if (n > max_size()) {
+            FAIL_EXIT("Requested memory size exceeds maximum allowed size");
+        }
+
+        void* p = sys_malloc(n * sizeof(T));
+        if (!p) {
+            FAIL_EXIT("bad alloc");
+        }
+        return static_cast<pointer>(p);
+    }
+
+    void deallocate(pointer p, size_type) { sys_free(p); }
+    void construct(pointer p, const value_type& x) { new(p) value_type(x); }
+    void destroy(pointer p) { p->~value_type(); }
+
+private:
+    size_type max_size() const throw() { return size_t(-1) / sizeof(T); }
+
+    void* (*sys_malloc)(size_t);
+    void (*sys_free)(void*);
+};
+
+template<typename T>
+inline bool operator==(const SysAlloc<T>&, const SysAlloc<T>&) {
+  return true;
+}
+
+template<typename T>
+inline bool operator!=(const SysAlloc<T>&, const SysAlloc<T>&) {
+  return false;
+}
+
 class Record {
 public:
     void host_alloc(void* ptr, size_t size);
@@ -55,7 +128,7 @@ public:
     void dma_alloc(int fd, size_t len);
     void dma_free(int fd);
     static Record& get_instance();
-    Record() : m_skip(5), m_file("memory_trace.txt") {}
+    Record() : m_skip(4), m_file("memory_trace.txt") {}
     ~Record() { 
         printf("\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
         printf("Host peak: %.2f MB, DMA peak: %.2f MB, Total peak: %.2f MB\n", 
@@ -68,8 +141,8 @@ private:
     size_t modify(size_t bias);
 
 private:
-    std::map<void*, size_t> m_host_bias;
-    std::map<int, size_t> m_dma_bias;
+    std::map<void*, size_t, std::less<void*>, SysAlloc<std::pair<void* const, size_t>>> m_host_bias;
+    std::map<int, size_t, std::less<int>, SysAlloc<std::pair<const int, size_t>>> m_dma_bias;
     Spinlock m_mutex;
     size_t m_dma_used = 0;
     size_t m_dma_peak = 0;
